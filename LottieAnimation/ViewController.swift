290e7332f4783117a1c6d47463f1822d7f6aa382
Heecheoljjang <kkll135@naver.com>
2022-07-10 22:33:27 +0900

    Initial Commit

---
+++ LottieAnimation/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  LottieAnimation
+//
+//  Created by HeecheolYoon on 2022/07/10.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "메인 화면"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    let animationView: AnimationView = {
+        let aniView = AnimationView(name: "111592-happy-pencil")
+        aniView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        //aniView.contentMode = .scaleToFill
+        return aniView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(animationView)
+        animationView.center = view.center
+        
+        animationView.play { _ in
+            self.view.addSubview(self.titleLabel)
+            self.animationView.removeFromSuperview()
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+            
+        }
+        
+        
+    }
+
+    
+
+}
+
